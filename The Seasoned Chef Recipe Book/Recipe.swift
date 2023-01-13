f02e839d2b0f1f685f33af4667647034fde060c5
smichaelonline <stephaniemichael001@gmail.com>
2023-01-13 15:15:31 -0500

    Add recipe and main information model

---
+++ The Seasoned Chef Recipe Book/Recipe.swift
@@ -0,0 +1,30 @@
+//
+//  Recipe.swift
+//  The Seasoned Chef Recipe Book
+//
+//  Created by Stephanie Michael on 1/13/23.
+//
+
+import Foundation
+
+
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [String]
+    var direction: [String]
+}
+
+struct MainInformation {
+    var name: String
+    var description: String
+    var author: String
+    var category: String
+    // categories are Breakfast, Lunch, Dinner, Dessert
+}
+
+enum Category: String, CaseIterable {
+    case breakfast = "Breakfast"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+    case dessert = "Dessert"
+}
