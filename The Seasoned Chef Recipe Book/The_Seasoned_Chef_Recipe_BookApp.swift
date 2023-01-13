c3ba0f6671039a94cf3e9a6807722f511db9337b
smichaelonline <stephaniemichael001@gmail.com>
2023-01-13 15:05:30 -0500

    Initial Commit

---
+++ The Seasoned Chef Recipe Book/The_Seasoned_Chef_Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Seasoned_Chef_Recipe_BookApp.swift
+//  The Seasoned Chef Recipe Book
+//
+//  Created by Stephanie Michael on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Seasoned_Chef_Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
