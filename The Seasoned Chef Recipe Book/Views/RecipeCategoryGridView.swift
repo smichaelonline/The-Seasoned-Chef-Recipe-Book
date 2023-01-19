bddfdea6cb3c346985ebcc9d3417e55b2be404d4
smichaelonline <stephaniemichael001@gmail.com>
2023-01-19 16:39:10 -0500

    Add images to categories

--- The Seasoned Chef Recipe Book/Views/RecipeCategoryGridView.swift
+++ The Seasoned Chef Recipe Book/Views/RecipeCategoryGridView.swift
@@ -7,4 +7,40 @@
 
 import SwiftUI
 
+struct RecipeCategoryGridView: View {
+    private var recipeData = RecipeData()
+    
+    var body: some View {
+        NavigationView{
+            ScrollView {
+                let columns = [GridItem(), GridItem()]
+                LazyVGrid(columns: columns, content:  {
+                    ForEach(MainInformation.Category.allCases, id: \.self) {
+                        category in CategoryView(category: category)
+                    }
+                })
+            }
+            .navigationTitle("Categories")
+        }
+    }
+}
 
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue).font(.title)
+        }
+    }
+}
+
+struct RecipeCategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryGridView()
+    }
+}
